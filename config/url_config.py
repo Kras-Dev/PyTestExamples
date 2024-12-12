8e908cf7a0123a0fe5dcff9451c32a3b124ec3d3
KrasDev <you@example.com>
2024-12-12 08:37:46 +0300

    Added url_config.py to manage URLs in the project

---
+++ config/url_config.py
@@ -0,0 +1,8 @@
+# config/url_config.py
+
+class Links:
+    HOST = "https://opensource-demo.orangehrmlive.com/web/index.php"
+    LOGIN_PAGE = f"{HOST}/auth/login"
+    DASHBOARD_PAGE = f"{HOST}/dashboard/index"
+    PERSONAL_PAGE = f"{HOST}/pim/viewPersonalDetails/empNumber/7"
+    BASE_URL = "https://petstore.swagger.io/v2"
